bf7cb9ab1cad725c7262abaec7960e512a13c1fa
Brotiger <dimka@bdima.ru>
2021-08-16 16:48:36 +0400

    fix img styles

--- resources/views/admin/ajax/category/imgBlock.blade.php
+++ resources/views/admin/ajax/category/imgBlock.blade.php
@@ -1,7 +1,7 @@
 @if($category->img)
     <div class="form-group">
         <div class="input-group">
-            <img src="{{ '/storage/' . $category->img }}" style="max-width: 100%">
+            <img src="{{ '/storage/' . $category->img }}" class="categoryImg">
         </div>
         <input type="hidden" name='deleteImg' value="false">
         <button type="button" class="btn btn-danger mt-2" btn-delete-img><i class="fas fa-trash"></i></button>
--- resources/views/admin/ajax/product/imgBlock.blade.php
+++ resources/views/admin/ajax/product/imgBlock.blade.php
@@ -1,7 +1,7 @@
 @foreach($product->images as $image)
     <div class="form-group">
         <div class="input-group">
-            <img src="{{ '/storage/' . $image->img }}" style="max-width: 100%">
+            <img src="{{ '/storage/' . $image->img }}" class="productImg">
         </div>
         <input type="hidden" name='deleteImg[{{ $image->id }}]' value="false" deleteImg>
         <button type="button" class="btn btn-danger mt-2" btn-delete-img><i class="fas fa-trash"></i></button>
